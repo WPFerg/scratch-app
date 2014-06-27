cfca06a0bfab539e4495e467198f1b87b5da56fe
William Ferguson <wferguson@scottlogic.com>
2014-06-27 12:09:25 +0100

    Merged styles over, text parsing, etc.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,8 +7,23 @@ angular.module('scratch.controllers', ['scratch.directives', 'ngRoute', 'ngResou
 
   }])
   .controller('ProjectCtrl', ['$scope', 'Projects', 'ProjectDetails', '$routeParams', function($scope, Projects, ProjectDetails, $routeParams) {
+  	
+  	// Entering a project URL navigates the project back to Index. Therefore this is commented out.
+
+  	// Check to see if the user's entered the Scratch website instead of a plain Project ID. If they have, parse it to get the project ID.
+  	// Example link http://scratch.mit.edu/projects/22564095/
+  	//
+  	// var projectId = $routeParams.projectId; 
+  	// // If the Project link appears,
+  	// if(projectId.indexOf("http://scratch.mit.edu/projects/") !== -1)
+  	// {
+  	// 	// Remove it
+  	// 	projectId = projectId.substring(32);
+  	// 	console.log(projectId);
+  	// }
+
   	// Get the project
-  	$scope.project = ProjectDetails.get({projectId: $routeParams.projectId});
+  	$scope.project = ProjectDetails.get({"projectId": $routeParams.projectId});
   	// This will then just be displayed on the view.
 
   }]);
--- app/js/filters.js
+++ app/js/filters.js
@@ -12,9 +12,43 @@ angular.module('scratch.filters', []).
   	{
   		return function(text)
   		{
+        // Quick type check to see if text is defined
+        if(typeof(text) === "undefined")
+        {
+          return;
+        }
   			// Replace \n with <br />
   			text = text.replace(/\n/g, "<br />");
   			// Trust the HTML so it can be displayed
   			return $sce.trustAsHtml(text);
   		}
-  	}]);
+  	}]).
+  filter("dateTimeToReadable", function()
+    { // Converts YYYY-MM-DDTHH:MM:SS into a readable format.
+      return function(text)
+      {
+        // Basic type check to see if text is defined
+        if(typeof(text) === "undefined")
+        {
+          return;
+        }
+
+        var date, time, day, month, year;
+        // Split into YYYY-MM-DD and HH:MM:SS
+        var dateTimeSplit = text.split(/T/);
+        date = dateTimeSplit[0];
+        time = dateTimeSplit[1];
+        // Time is in readable format, just need to convert date
+
+        // Split into YYYY, MM, and DD
+        var dateComponents = date.split(/-/);
+        year = dateComponents[0];
+        month = dateComponents[1];
+        day = dateComponents[2];
+
+        // Compose a readable date formate, DD/MM/YYYY.
+        date = day + "/" + month + "/" + year;
+
+        return date + " " + time;
+      }
+    });
