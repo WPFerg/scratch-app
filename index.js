b7e6f9d528ece1a71cf12a75cfecc2e97b8053e7
William Ferguson <wferguson@scottlogic.com>
2014-07-02 09:53:32 +0100

    Scratch player now dynamically loads manifest via templates.

--- index.js
+++ index.js
@@ -1,51 +1,27 @@
-var connect = require('connect');
+var express = require('express');
+var ejs = require('ejs');
 var http = require('http');
 
 var url = require('url');
 var proxy = require('proxy-middleware');
 
+var scratch = require("./scratch-folder-middleware");
 var manifest = require("./manifest");
 var fs = require('fs');
 
-var app = connect()
-  .use(connect.logger('dev'));
+var app = express()
+  //.use(express.logger('dev'));
 
+// Set the dynamic HTML renderer as EJS.
+// This is for the scratch-player folder to dynamically generate the HTML for the manifest.
+app.engine('html', ejs.renderFile);
+// Set the views for EJS to be in the HTML player folder, since that's the only place EJS needs to know about.
+app.set('views', __dirname + "/scratch-player")
 app.use('/projects/', proxy(url.parse('http://projects.scratch.mit.edu/internalapi/project/')));
 app.use('/projectdetails/', proxy(url.parse('http://scratch.mit.edu/api/v1/project/')));
 app.use('/asset/', proxy(url.parse('http://cdn.scratch.mit.edu/internalapi/asset/')));
-app.use('/scratch-player/', function(req, res) {
-	// Get the URL request
-	var url = req.url.substring(1);
-
-	// If the url requests a folder, return that.
-	if(url.indexOf(".") !== -1 || url.indexOf("/") !== -1)
-	{
-		// Get the file info and return it to the client
-		fs.readFile("scratch-player/" + url, function (err, data) {
-			if(err)
-			{
-				data = "System Error -- Can't get file details";
-			}
-			res.end(data);
-		});
-	} else {
-		// Otherwise return the index with the dynamic manifest generated.
-
-		fs.readFile("scratch-player/index.html", function (err, data) {
-			var html;
-			if(!err)
-			{
-				// And get the index file and concatenate that with the manifest <html>
-				html = "<!DOCTYPE html><html manifest='/manifest/" + url + "'><head><script>var projectId = " + url + ";</script>" + data;
-			} else {
-				console.log(err);
-				html = "System Error -- Can't get Player HTML";
-			}
-			res.end(html);
-		})
-	}
-});
-app.use(connect.static('.'));
+app.use('/scratch-player/', scratch.serveScratchFolder);
+app.use(express.static('.'));
 app.use('/manifest', function(req, res) {
 
 
@@ -57,10 +33,10 @@ app.use('/manifest', function(req, res) {
 
 	if(parseInt(projectId).toString().length == 8)
 	{
+		// Return the response as plain text cache manifest
+		res.setHeader("Content-Type", "text/cache-manifest");
 		// Project ID is a number, create the manifest.
 		manifest.createManifest(projectId, function(manifest) {
-			// Return the response as plain text cache manifest
-			res.setHeader("Content-Type", "text/cache-manifest");
 			res.end(manifest);
 		});
 	} else {
