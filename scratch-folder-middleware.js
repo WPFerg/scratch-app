709785bb6ecfd9d1f7c26a4cb4b9be47d6b6bf96
William Ferguson <wferguson@scottlogic.com>
2014-07-02 10:42:01 +0100

    Rephrased the project doesn't exist.

--- scratch-folder-middleware.js
+++ scratch-folder-middleware.js
@@ -1,46 +1,6 @@
 // Serve Player HTML
 var fs = require('fs');
 
-// exports.serveScratchFolder = function(req, res)
-// {
-// 	// Get the URL request
-// 	var url = req.url.substring(1);
-// 	// Response to the client. 200 is a success.
-// 	var responseCode = 200;
-
-// 	// If the url requests a folder, return that.
-// 	if(url.indexOf(".") !== -1 || url.indexOf("/") !== -1)
-// 	{
-// 		// Get the file info and return it to the client
-// 		fs.readFile("scratch-player/" + url, function (err, data) {
-// 			if(err)
-// 			{
-// 				// Internal server error 500
-// 				responseCode = 500;
-// 				data = "System Error -- Can't get file details";
-// 			}
-// 			res.end(responseCode, data);
-// 		});
-// 	} else {
-// 		// Otherwise return the index with the dynamic manifest generated.
-
-// 		fs.readFile("scratch-player/index.html", function (err, data) {
-// 			var html;
-// 			if(!err)
-// 			{
-// 				// And get the index file and concatenate that with the manifest <html>
-// 				// VERY inelegant
-// 				html = "<!DOCTYPE html><html manifest='/manifest/" + url + "'><head><script>var projectId = " + url + ";</script>" + data;
-// 			} else {
-// 				// Internal server error 500
-// 				responseCode = 500;
-// 				html = "System Error -- Can't get Player HTML";
-// 			}
-// 			res.end(responseCode, html);
-// 		})
-// 	}
-// }
-
 exports.serveScratchFolder = function(req, res)
 {
 	// Get the URL request
