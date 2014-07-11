87a40f5b4ea7cc61b38ee1b5b69da7e3485ff000
William Ferguson <wferguson@scottlogic.com>
2014-07-11 09:38:08 +0100

    iPad users no longer get the soundbank files.

--- test/node-server-test.js
+++ test/node-server-test.js
@@ -138,7 +138,7 @@ describe("Manifest", function() {
 	var manifestData, done;
 
 	it("should refer to the Scratch API that has sounds, costumes, children as part of its JSON object", function(done) {
-		manifest.createManifest(23600596, function (data) {
+		manifest.createManifest(23600596, "", function (data) {
 			// Check to see if a manifest's created.
 			expect(data.indexOf("CACHE MANIFEST")).toBe(0);
 			done();
@@ -146,7 +146,7 @@ describe("Manifest", function() {
 	});
 
 	it("should not generate a manifest from an invalid id", function(done) {
-		manifest.createManifest("", function (data) {
+		manifest.createManifest("", "", function (data) {
 			// Check to see if a manifest's created.
 			expect(data.indexOf("CACHE MANIFEST")).toBe(-1);
 			done();
@@ -160,45 +160,231 @@ describe("Manifest", function() {
 							 [{"sounds": [{"md5":"ImASound.mp3"}], "costumes": [{"baseLayerMD5":"ImACostume.jpg"}]},
 							  {"sounds": [{"md5":"ImASound2.mp3"}], "costumes": [{"baseLayerMD5":"ImACostume3.jpg"}]} ] }
 
-			manifest.generateManifest("TestManifest", input, function(data)
+			manifest.generateManifest("TestManifest", true, input, function(data)
 			{
 				// Store the manifest data, run a quick check to see if it exists.
 				manifestData = data;
 				expect(manifestData).not.toBeUndefined();
-				expect(data.indexOf("CACHE MANIFEST")).not.toBe(-1);
+				expect(manifestData.indexOf("soundbank")).not.toBe(-1);
+				expect(manifestData.indexOf("CACHE MANIFEST")).not.toBe(-1);
 				done();
 			});
 	});
 
 	it("should generate the correct manifest based on that input", function() {
 		// Check to see if the files are in the manifest
-		expect(manifestData.indexOf("projects/TestManifest/get/")).not.toBe(-1);
-		expect(manifestData.indexOf("asset/ImASound.mp3/get/")).not.toBe(-1);
-		expect(manifestData.indexOf("asset/ImACostume.jpg/get/")).not.toBe(-1);
-		expect(manifestData.indexOf("asset/ImASound2.mp3/get/")).not.toBe(-1);
-		expect(manifestData.indexOf("asset/ImACostume3.jpg/get/")).not.toBe(-1);
+		expect(manifestData.indexOf("http://projects.scratch.mit.edu/internalapi/project/TestManifest/get/")).not.toBe(-1);
+		expect(manifestData.indexOf("http://cdn.scratch.mit.edu/internalapi/asset/ImASound.mp3/get/")).not.toBe(-1);
+		expect(manifestData.indexOf("http://cdn.scratch.mit.edu/internalapi/asset/ImACostume.jpg/get/")).not.toBe(-1);
+		expect(manifestData.indexOf("http://cdn.scratch.mit.edu/internalapi/asset/ImASound2.mp3/get/")).not.toBe(-1);
+		expect(manifestData.indexOf("http://cdn.scratch.mit.edu/internalapi/asset/ImACostume3.jpg/get/")).not.toBe(-1);
 	})
 
 	it("should not have duplicate entries in the manifest", function() {
 		// Check to see if these aren't duplicated. Remove the first (hopefully only) instance these occur and then check to see if another exists.
-		manifestData = manifestData.replace("projects/TestManifest/get/","");
-		manifestData = manifestData.replace("asset/ImASound.mp3/get/","");
-		manifestData = manifestData.replace("asset/ImACostume.jpg/get/","");
-		manifestData = manifestData.replace("asset/ImASound2.mp3/get/","");
-		manifestData = manifestData.replace("asset/ImACostume3.jpg/get/","");
+		manifestData = manifestData.replace("http://projects.scratch.mit.edu/internalapi/project/TestManifest/get/","");
+		manifestData = manifestData.replace("http://cdn.scratch.mit.edu/internalapi/asset/ImASound.mp3/get/","");
+		manifestData = manifestData.replace("http://cdn.scratch.mit.edu/internalapi/asset/ImACostume.jpg/get/","");
+		manifestData = manifestData.replace("http://cdn.scratch.mit.edu/internalapi/asset/ImASound2.mp3/get/","");
+		manifestData = manifestData.replace("http://cdn.scratch.mit.edu/internalapi/asset/ImACostume3.jpg/get/","");
 
 
-		expect(manifestData.indexOf("projects/TestManifest/get/")).toBe(-1);
-		expect(manifestData.indexOf("asset/ImASound.mp3/get/")).toBe(-1);
-		expect(manifestData.indexOf("asset/ImACostume.jpg/get/")).toBe(-1);
-		expect(manifestData.indexOf("asset/ImASound2.mp3/get/")).toBe(-1);
-		expect(manifestData.indexOf("asset/ImACostume3.jpg/get/")).toBe(-1);
+		expect(manifestData.indexOf("http://projects.scratch.mit.edu/internalapi/project/TestManifest/get/")).toBe(-1);
+		expect(manifestData.indexOf("http://cdn.scratch.mit.edu/internalapi/asset/ImASound.mp3/get/")).toBe(-1);
+		expect(manifestData.indexOf("http://cdn.scratch.mit.edu/internalapi/asset/ImACostume.jpg/get/")).toBe(-1);
+		expect(manifestData.indexOf("http://cdn.scratch.mit.edu/internalapi/asset/ImASound2.mp3/get/")).toBe(-1);
+		expect(manifestData.indexOf("http://cdn.scratch.mit.edu/internalapi/asset/ImACostume3.jpg/get/")).toBe(-1);
 
+	});
+
+	it("should generate without a sound bank on an iPad", function(done)
+	{
+	// Create a HTTP GET request with the text file as a parameter
+		var requestOpts = {
+			host: "localhost",
+			port: 3000,
+			path: "/manifest/23600596",
+			headers: {"User-Agent": "iPad"}
+		};
+
+		// Create a function that parses the responses from the HTTP request
+		var responseCallback = function (response) {
+			var str = "";
+
+			// If there's data, add that chunk to the current store
+			response.on('data', function (chunk) {
+				str += chunk;
+			});
+
+			// If there's no more data, check to see if the response is correct, and signify the test's done.
+			response.on('end', function() {
+				expect(str.indexOf("soundbank")).toBe(-1);
+				done();
+			});
+
+			// If there's an error, fail the test.
+			response.on('error', function() {
+				expect().toFail();
+				done();
+			});
+		}
+
+		// Create and send the request
+		http.request(requestOpts, responseCallback).end();
+		
 	});
 
 });
 
 describe("User Details", function() {
 
+	it("should get a user's projects", function(done) {
+			// Create a HTTP GET request with the Username as a parameter
+		var requestOpts = {
+			host: "localhost",
+			port: 3000,
+			path: "/user/projects/WillJimbo"
+		};
+
+		// Create a function that parses the responses from the HTTP request
+		var responseCallback = function (response) {
+			var str = "";
+
+			// If there's data, add that chunk to the current store
+			response.on('data', function (chunk) {
+				str += chunk;
+			});
+
+			// If there's no more data, check to see if the response is correct, and signify the test's done.
+			response.on('end', function() {
+				str = JSON.parse(str);
+				expect(str.success).toBe(200);
+				expect(str.projects).not.toBeUndefined();
+				done();
+			});
+
+			// If there's an error, fail the test.
+			response.on('error', function() {
+				expect().toFail();
+				done();
+			});
+		}
+
+		// Create and send the request
+		http.request(requestOpts, responseCallback).end();
+
+	});
+
+	it("should not get an invalid user's projects", function(done) {
+			// Create a HTTP GET request with the Username as a parameter
+		var requestOpts = {
+			host: "localhost",
+			port: 3000,
+			path: "/user/projects/WillJambo"
+		};
+
+		// Create a function that parses the responses from the HTTP request
+		var responseCallback = function (response) {
+			var str = "";
+
+			// If there's data, add that chunk to the current store
+			response.on('data', function (chunk) {
+				str += chunk;
+			});
+
+			// If there's no more data, check to see if the response is correct, and signify the test's done.
+			response.on('end', function() {
+				str = JSON.parse(str);
+				expect(str.error).toBe(404);
+				expect(str.projects).toBeUndefined();
+				done();
+			});
+
+			// If there's an error, fail the test.
+			response.on('error', function() {
+				expect().toFail();
+				done();
+			});
+		}
+
+		// Create and send the request
+		http.request(requestOpts, responseCallback).end();
+
+	});
+
+
+	it("should get a user's followers", function(done) {
+			// Create a HTTP GET request with the Username as a parameter
+		var requestOpts = {
+			host: "localhost",
+			port: 3000,
+			path: "/user/followers/WillJimbo"
+		};
+
+		// Create a function that parses the responses from the HTTP request
+		var responseCallback = function (response) {
+			var str = "";
+
+			// If there's data, add that chunk to the current store
+			response.on('data', function (chunk) {
+				str += chunk;
+			});
+
+			// If there's no more data, check to see if the response is correct, and signify the test's done.
+			response.on('end', function() {
+				str = JSON.parse(str);
+				expect(str.success).toBe(200);
+				expect(str.followers).not.toBeUndefined();
+				done();
+			});
+
+			// If there's an error, fail the test.
+			response.on('error', function() {
+				expect().toFail();
+				done();
+			});
+		}
+
+		// Create and send the request
+		http.request(requestOpts, responseCallback).end();
+
+	});
+
+	it("should not get an invalid user's followers", function(done) {
+			// Create a HTTP GET request with the Username as a parameter
+		var requestOpts = {
+			host: "localhost",
+			port: 3000,
+			path: "/user/followers/WillJambo"
+		};
+
+		// Create a function that parses the responses from the HTTP request
+		var responseCallback = function (response) {
+			var str = "";
+
+			// If there's data, add that chunk to the current store
+			response.on('data', function (chunk) {
+				str += chunk;
+			});
+
+			// If there's no more data, check to see if the response is correct, and signify the test's done.
+			response.on('end', function() {
+				str = JSON.parse(str);
+				expect(str.error).toBe(404);
+				expect(str.followers).toBeUndefined();
+				done();
+			});
+
+			// If there's an error, fail the test.
+			response.on('error', function() {
+				expect().toFail();
+				done();
+			});
+		}
+
+		// Create and send the request
+		http.request(requestOpts, responseCallback).end();
+
+	});
 
 });
