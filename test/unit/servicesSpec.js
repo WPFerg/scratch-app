bc1be52b5ff3cdc8be93f31a9bba89db86100602
JMVHill <james.hill@lcsltd.co.uk>
2014-07-14 15:41:02 +0100

    Fixed test issues.

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -5,13 +5,6 @@
 describe('service', function() {
   beforeEach(module('scratch.services'));
 
-
-  describe('version', function() {
-    it('should return current version', inject(function(version) {
-      expect(version).toEqual('0.1');
-    }));
-  });
-
  // ***  Services tested in controllers.  *** //
 
   // describe("Projects", function() {
@@ -32,5 +25,6 @@ describe('service', function() {
   //     $httpBackend.flush();
 
   //   });
-  });
+  //});
+
 });
