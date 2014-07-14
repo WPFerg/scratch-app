c41c7cb0a72b5f3f8eb72823224cee2ea12540c2
William Ferguson <wferguson@scottlogic.com>
2014-07-14 14:30:43 +0100

    Added <label> tag.

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,25 +1,25 @@
 'use strict';
- 
+
 /* Directives */
- 
+
 // Create transaction visualisation
 var scratch = angular.module('scratch.directives', ['ngRoute']);
- 
- 
+
+
 // Version notice directive
 scratch.directive('getAppVersion', function()
 {
     return {
-                restrict: 'E',
-                scope:
-                {
- 
-                },
-                link: function(scope, element)
-                {
- 
-                }
-                }
+    	restrict: 'E',
+    	scope:
+    	{
+
+    	},
+    	link: function(scope, element)
+    	{
+
+    	}
+  	}
 });
- 
+
 // Copyright notice directive
