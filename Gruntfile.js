c61842c173bfda0b568bdcb2f5d0122a2e3d4422
William Ferguson <wferguson@scottlogic.com>
2014-07-24 15:20:59 +0100

    Updated the grunt file to hide some JSHint results

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,10 +25,52 @@ module.exports = function(grunt)
         }
       },
       jshint: {
-        src: ["*.js", "scratch-player/**/*.js", "app/**/*.js"],
         options: {
-          reporterOutput: "js-hint-results.txt"
-        }
+          reporterOutput: "js-hint-results.txt",
+          "-W097": false, // 'use strict' as a function
+          "-W041": false, // == null
+          "-W069": false, // dot notation over ['Something']
+          undef: false, // x,y,z is undefined
+          eqeqeq: false, // === over ==
+          eqnull: false,
+          asi: true,
+          globals: {
+            "$": false,
+            "runtime":false,
+            "Runtime":false,
+            "XMLHttpRequest":false,
+            "SoundDecoder":false,
+            "Instr":false,
+            "OffsetBuffer":false,
+            "SoundPrims":false,
+            "SensingPrims":false,
+            "Stage":false,
+            "Sprite":false,
+            "List":false,
+            "Reporter":false,
+            "interp":false,
+            "Interpreter":false,
+            "WAVFile":false,
+            "window":false,
+            "document": false,
+            "Point": false,
+            "Color":false,
+            "Rectangle":false,
+            "Timer":false,
+            "Thread":false,
+            "console":false,
+            "io":false,
+            "IO":false,
+            "Primitives":false,
+            "setTimeout":false,
+            "setInterval":false,
+            "Scalar":false,
+            "SoundBank":false,
+            "projectId":false
+          }
+        },
+        src: ["*.js", "scratch-player/js/**/*.js", "app/*.js"],
+        exclude: ["**/bower_components/**/*.js"],
       }
 
     });
